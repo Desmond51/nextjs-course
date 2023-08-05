72cd7b597df9b4dde4d0f57f1b25d57c8a45ee21
Desmond51 <desmondtekum9@gmail.com>
2023-08-05 19:12:11 +0100

    Changes on the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,10 +6,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-        <Link href="/" className='flex gap-2 flex-center'>
-            <Image src= "/assets/images/logo.svg" width={30} height={30} alt='Promptopia Logo' className='object-contain'  />
-            <p className='logo_text'>Promptopia</p>
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex gap-2 flex-center">
+            <Image src= "/assets/images/logo.svg" width={30} height={30} alt="Promptopia Logo" className="object-contain " />
+            <p className="logo_text"> Promptopia</p>
         </Link>
     </nav>
   )
