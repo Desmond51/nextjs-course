b6f871a961979db6cd90b1ad373f34971114ccbb
Desmond51 <desmondtekum9@gmail.com>
2023-08-05 19:04:04 +0100

    Creating various components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Navbar from '@components/Navbar'
 import '@styles/global.css'
 
 export const metadata = {
@@ -9,11 +10,12 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-      
+     
       <div className="main">
         <div className="gradient" />
       </div>
       <main className="app">
+      <Navbar />
         {children}
       
       </main>
---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+" use client";
+import { use } from 'bcrypt/promises'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'>
+            <Image src= "/assets/images/logo.svg" width={30} height={30} alt='Promptopia Logo' className='object-contain'  />
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
